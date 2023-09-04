d8c5a1e2e8ca66092b20e909cd86f84de1ba5773
MrMvshv <machariajosphat001@gmail.com>
2023-09-04 13:33:52 +0000

    mongodb class

---
+++ utils/db.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    this.client.connect((err) => {
+      if (err) {
+        console.log('Connection to MongoDB failed:', err);
+      } else {
+        console.log('Connection to MongoDB successful');
+      }
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const db = this.client.db();
+    const usersCollection = db.collection('users');
+    const count = await usersCollection.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    const db = this.client.db();
+    const filesCollection = db.collection('files');
+    const count = await filesCollection.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
